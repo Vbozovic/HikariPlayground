4fdd92e596388ac99af503396b1a798a4727a436
vbozovic <vbozovic14@yahoo.com>
2023-06-19 23:29:39 +0200

    Initial commit

---
+++ src/main/java/com/example/hikari/demo/HikariPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.hikari.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikariPlaygroundApplication.class, args);
+	}
+
+}
